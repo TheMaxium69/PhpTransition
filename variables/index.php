9f43662ff3e4f45e07c3ed94f83ab03da66fa461
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-05-27 11:07:38 +0200

    suppTableau

--- variables/index.php
+++ variables/index.php
@@ -9,7 +9,18 @@ echo $Tableau[4]. "<br>";
 //Add Simple
 $Tableau[] = "testSimple";
 echo $Tableau[5]. "<br>";
+var_dump($Tableau);
+echo "Le Tableau est égal a ". count($Tableau);
+
+//Supp Unset
+unset($Tableau[4]);
+
+
+//Supp Simple
+//$Tableau[5] = "";
+
 
 //Affichage
 var_dump($Tableau);
+echo "Le Tableau est égal a ". count($Tableau);
 ?>
