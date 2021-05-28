832ddc6b940ba2f18280ccbb79d3e6b2a7369efc
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-05-28 11:42:28 +0200

    verifmdp

--- secret/form.php
+++ secret/form.php
@@ -8,22 +8,14 @@
 </head>
 <body>
 
-<form class="row g-3">
+<form method="get" class="row g-3">
     <div class="col-auto">
-        <input type="text" class="form-control" id="" placeholder="Mot de passe">
+        <input type="text" class="form-control" name="mdp" id="" placeholder="Mot de passe">
     </div>
     <div class="col-auto">
         <button type="submit" class="btn btn-success">OK</button>
     </div>
 </form>
-
-<D>couou</D>
-<p>ciao</p>
-<p>couou</p>
-<p>ciao</p>
-<p>couou</p>
-<p>ciao</p>
-
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- secret/index.php
+++ secret/index.php
@@ -13,14 +13,15 @@ $modeRevelation = false;
 $modeForm = true;
 $secret = "le sens de la vie c'est       ";
 $revelation = "c'est à cet endroit que le secret devrait être revelé";
-$unForm = '<form class="row g-3">
+$unForm = '<form method="get" class="row g-3">
                 <div class="col-auto">
-                    <input type="text" class="form-control" id="" placeholder="Mot de passe">
+                    <input type="text" class="form-control" name="pass" id="" placeholder="Mot de passe">
                 </div>
                 <div class="col-auto">
                     <button type="submit" class="btn btn-success">OK</button>
                 </div>
            </form>';
+$password = "carrotte";
 
 
 if($modeRevelation){
@@ -31,18 +32,34 @@ if($modeRevelation){
     echo "[debug] Faux tu peut pas voir le secret". "<br>";
 }
 
+if($_GET){
+    echo "[debug] Get Existe". "<br>";
+    $modeForm = !$modeForm;
+
+    if($_GET['pass'] == $password){
+        echo "[debug] Mot de passe bon". "<br>";
+    }else{
+        echo "[debug] Mot de passe pas bon". "<br>";
+    }
+}else{
+    echo "[debug] Get Existe Pas". "<br>";
+}
+
+
 if(!$modeForm){
     echo "[debug] Désactivé le formulaire". "<br>";
     $unForm = "";
 }else {
     echo "[debug] Activé le formulaire". "<br>";
 }
+
 ?>
 </div>
 <br>
 
-<?php echo $revelation; ?>
-<?php echo $unForm; ?>
+<?php echo $revelation;
+      echo $unForm;?>
+<hr>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
